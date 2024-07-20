61db319bd9b351f76cdb4b1b4b6ee8a479b3e394
Wilian Morales <willian.moralesch@gmail.com>
2024-07-20 00:08:01 -0500

    Culmination of Angular pipes.

--- src/app/products/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/products/pages/uncommon-page/uncommon-page.component.ts
@@ -1,15 +1,81 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-uncommon-page',
   templateUrl: './uncommon-page.component.html',
-  styleUrls: ['./uncommon-page.component.css']
+  styles: []
 })
-export class UncommonPageComponent implements OnInit {
+export class UncommonPageComponent {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  // i18nSellect
+  public name: string = 'Willian';
+  public gender: 'male'|'female' = 'male';
+  public invitationMap = {
+    'male': 'invitarlo',
+    'female': 'invitarla'
   }
+
+  changeClient(): void {
+    this.name = 'Nicole';
+    this.gender = 'female';
+  }
+
+  // i18nPlural
+  public clients: string[] = ['Maria', 'Pedro', 'Jose', 'Miguel', 'Nicole', 'Jhon'];
+  public clientsMap = {
+    '=0': 'no tenemos nínguna persona esperando.',
+    '=1': 'tenemos una persona esperando.',
+    '=2': 'tenemos 2 personas esperando.',
+    'other': 'tenemos # personas esperando.',
+  }
+
+  deleteClient(): void {
+    this.clients.shift();
+  }
+
+  // Slice
+  str: string = 'abcdefghij';
+
+  // JsonPipe
+  public heroes: Object = [
+    {
+      name: 'Batman',
+      fly: false
+    },
+    {
+      name: 'Superman',
+      fly: true,
+      powers: [
+        'invencibility',
+        'heat vision'
+      ]
+    },
+    {
+      name: 'Thor',
+      fly: true
+    }
+  ]
+
+  //KeyValue Pípe
+  public person = {
+    name: 'Willian Morales',
+    age: 25,
+    addres: 'Trujillo, Perú',
+  }
+
+  // Async Pipe
+  public myObservableTimer = interval(2000)
+    .pipe(
+      tap( value => value
+      )
+    );
+
+    public promiseValue: Promise<string> = new Promise(  (resolve, reject) => {
+      setTimeout(() => {
+        resolve('Tenemos data en la promesa');
+      }, 3500);
+    })
 
 }
